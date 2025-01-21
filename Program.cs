8a87dac1386343a8b34de944c974dafe5b21e91f
richard <richard.h.passos@hotmail.com>
2025-01-21 00:36:09 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using dotenv.net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Carregar o arquivo de configuração .env
+DotEnv.Load();
+var port = Environment.GetEnvironmentVariable("PORT");
+
+var app = builder.Build();
+
+// Configure um endpoint GET que retorna "Hello World"
+app.MapGet("/api/helloworld", () => "Hello World");
+
+// Usar a porta configurada no .env
+app.Run($"http://localhost:{port}");
